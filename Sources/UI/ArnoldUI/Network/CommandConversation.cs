f811427f66a7970e2f7399847c63efae5cbb7a90
Honza Sinkora <jan.sinkora@keenswh.com>
2016-04-11 16:15:45 +0200

    Added conversation classes for GetState and Command requests

---
+++ Sources/UI/ArnoldUI/Network/CommandConversation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.Arnold.Network;
+
+namespace ArnoldUI.Network
+{
+    public class CommandConversation : IConversation<CommandRequest, StateResponse>
+    {
+        public string Handler => "CommandHandler";
+        public CommandRequest Request { get; }
+
+        public CommandConversation()
+        {
+            Request = new CommandRequest();
+        }
+    }
+}
---
+++ Sources/UI/ArnoldUI/Network/GetStateConversation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.Arnold.Network;
+
+namespace ArnoldUI.Network
+{
+    public class GetStateConversation : IConversation<GetStateRequest, StateResponse>
+    {
+        public string Handler => "GetStateHandler";
+        public GetStateRequest Request { get; }
+
+        public GetStateConversation()
+        {
+            Request = new GetStateRequest();
+        }
+    }
+}
