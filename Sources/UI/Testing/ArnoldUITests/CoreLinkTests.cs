69372dbef98f53171dccb5a7aa1b074aac1c1019
Honza Sinkora <jan.sinkora@keenswh.com>
2016-04-11 14:20:29 +0200

    Switching from Rhinomock to Moq

--- Sources/UI/Testing/ArnoldUITests/CoreLinkTests.cs
+++ Sources/UI/Testing/ArnoldUITests/CoreLinkTests.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using ArnoldUI.Network;
 using GoodAI.Arnold.Net;
 using GoodAI.Net.ConverseSharp;
-using Rhino.Mocks;
+using Moq;
 using Xunit;
 
 namespace GoodAI.Arnold.UI.Tests
@@ -45,9 +45,10 @@ namespace GoodAI.Arnold.UI.Tests
                 }
             };
 
-            var converseClient = MockRepository.GenerateMock<IConverseProtoBufClient>();
-            converseClient.Stub(client => client.SendQuery<CommandRequest, StateResponse>(conv.Handler, conv.Request))
-                .Return(response);
+            var converseClientMock = new Mock<IConverseProtoBufClient>();
+            converseClientMock.Setup(client => client.SendQuery<CommandRequest, StateResponse>(conv.Handler, conv.Request))
+                .Returns(response);
+            IConverseProtoBufClient converseClient = converseClientMock.Object;
 
             var coreLink = new CoreLink(converseClient);
 
