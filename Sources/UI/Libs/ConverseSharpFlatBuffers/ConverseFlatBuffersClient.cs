b96a06a05dad8d0132d69fe52532ba1eed6851c2
Honza Sinkora <jan.sinkora@keenswh.com>
2016-04-19 15:53:38 +0200

    Added the FlatBuffers client project

---
+++ Sources/UI/Libs/ConverseSharpFlatBuffers/ConverseFlatBuffersClient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.Net.ConverseSharp;
+
+namespace GoodAI.Net.ConverseSharpFlatBuffers
+{
+    public interface IConverseFlatBuffersClient
+    {
+    }
+
+    public class ConverseFlatBuffersClient : ConverseClient, IConverseFlatBuffersClient
+    {
+        public ConverseFlatBuffersClient(ITcpConnector connector) : base(connector)
+        { }
+    }
+}
