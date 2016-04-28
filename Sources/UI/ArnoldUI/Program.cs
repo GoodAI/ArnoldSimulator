c9a8ea13a90e0f3b674e97b32dd4c8bce41c8854
PremekPaska <premysl.paska@keenswh.com>
2016-04-28 15:58:06 +0200

    Put logging setup into a separate file.

--- Sources/UI/ArnoldUI/Program.cs
+++ Sources/UI/ArnoldUI/Program.cs
@@ -20,62 +20,13 @@ namespace GoodAI.Arnold
         {
             UnhandledExceptionCatcher.RegisterHandlers();
 
-            SetupLogging();
+            LoggingConfig.Setup();
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
         }
 
-        private static void SetupLogging(string logPath = null)
-        {
-            try
-            {
-                string appName = typeof(Program).Namespace ?? "(default)";
 
-                if (string.IsNullOrEmpty(logPath))
-                {
-                    // Log to 'GoodAI\Arnold\Logs'.
-                    logPath = InitLogDirectory(appName.Replace('.', '\\'), "Logs");
-                }
-
-                SerilogRobeConfig.SetupLoggingToFile(logPath, appName);
-
-                UnhandledExceptionCatcher.Log = SerilogRobe.CreateLogger();
-            }
-            catch (Exception ex)
-            {
-                // TODO: Print inner exceptions and stack traces
-                MessageBox.Show(
-                    "Logging setup failed. The application will not log.\n\n" +
-                    $"Attempted log path: {logPath}\n\nException message: {ex.Message}",
-                    "Logging initialization error.", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-            }
-        }
-
-        private static string InitLogDirectory(string subfolderPath, string logFolder)
-        {
-            string logPath = Path.Combine(GetDefaultLogPath(), subfolderPath, logFolder);
-
-            Directory.CreateDirectory(logPath);
-
-            return logPath;
-        }
-
-        private static string GetDefaultLogPath()
-        {
-            try
-            {
-                return Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-            }
-            catch (Exception)
-            {
-                string path = Path.GetTempPath();
-
-                MessageBox.Show($"Error getting application data path. Logging to alternative path: {path}");
-
-                return path;
-            }
-        }
     }
 }
