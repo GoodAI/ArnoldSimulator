c2f6745f51ae3e9ff6ecf866ea1385394bfbe759
Honza Sinkora <jan.sinkora@keenswh.com>
2016-05-13 16:24:50 +0200

    Added configuration verification test

---
+++ Sources/UI/Testing/ArnoldUITests/ConfigurationTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleInjector;
+using Xunit;
+
+namespace GoodAI.Arnold.UI.Tests
+{
+    public class ConfigurationTests
+    {
+        [Fact]
+        public void VerifiesContainerConfiguration()
+        {
+            var container = new Container();
+            new ArnoldContainerConfig().Configure(container);
+            container.Verify();
+        }
+    }
+}
