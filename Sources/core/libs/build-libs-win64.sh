5086fb2b79d22659684a491733ce27a1541d1cc1
PremekPaska <premysl.paska@keenswh.com>
2016-05-27 16:29:31 +0200

    Libs: cleanup build script.
    Libs: Patch sparsehash patching, add some error handling.
    
    Libs: Teach the build script to say something when run without arguments.

--- Sources/core/libs/build-libs-win64.sh
+++ Sources/core/libs/build-libs-win64.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+die()
+{
+    echo "Fatal error. Aborting."
+    exit 1
+}
+
 build_charm()
 {
     echo "Building Charm++"
@@ -9,13 +15,13 @@ build_charm()
     echo "...cleaning"
     rm -r -f charm
 
-    if [ `ls -1 charm-$CHARM_VERSION.tar.gz 2>/dev/null | wc -l` -le 0 ]
+    if [ ! -f "charm-${CHARM_VERSION}.tar.gz" ]
     then
-        wget --no-check-certificate --output-document=charm-$CHARM_VERSION.tar.gz $CHARM_LINK
+        wget --no-check-certificate --output-document="charm-${CHARM_VERSION}.tar.gz" $CHARM_LINK
     fi
 
     echo "...unpacking"
-    tar --extract --gzip --file=charm-$CHARM_VERSION.tar.gz
+    tar --extract --gzip --file="charm-${CHARM_VERSION}.tar.gz"
     mv charm-$CHARM_VERSION charm
     cd charm
 
@@ -37,20 +43,10 @@ build_charm()
 build_tbb()
 {
     echo "Building TBB"
-    TBB_VERSION=44_20160413
-    TBB_LINK=https://www.threadingbuildingblocks.org/sites/default/files/software_releases/windows/tbb44_20160413oss_win.zip
+    local TBB_VERSION=44_20160413
+    local TBB_LINK="https://www.threadingbuildingblocks.org/sites/default/files/software_releases/windows/tbb${TBB_VERSION}oss_win.zip"
 
-    echo "...cleaning"
-    rm -r -f tbb
-
-    if [ `ls -1 tbb${TBB_VERSION}oss_win.zip 2>/dev/null | wc -l` -le 0 ]
-    then
-        wget --no-check-certificate --output-document=tbb${TBB_VERSION}oss_win.zip $TBB_LINK
-    fi
-
-    echo "...unpacking"
-    unzip -q -n tbb${TBB_VERSION}oss_win.zip
-    mv tbb${TBB_VERSION}oss tbb
+    download_and_unzip "$TBB_LINK" "tbb" "$TBB_VERSION" "tbb${TBB_VERSION}oss"
 
     cp -f -r tbb/bin/intel64/vc14/* tbb/bin
     cp -f -r tbb/lib/intel64/vc14/* tbb/lib
@@ -59,26 +55,20 @@ build_tbb()
 build_sparsehash()
 {
     echo "Building sparsehash"
-    SPARSEHASH_VERSION=2.0.3
-    SPARSEHASH_LINK=https://github.com/sparsehash/sparsehash/archive/sparsehash-2.0.3.zip
+    local SPARSEHASH_VERSION=2.0.3
+    local SPARSEHASH_LINK="https://github.com/sparsehash/sparsehash/archive/sparsehash-${SPARSEHASH_VERSION}.zip"
 
-    echo "...cleaning"
-    rm -r -f sparsehash
+    download_and_unzip "$SPARSEHASH_LINK" "sparsehash" "$SPARSEHASH_VERSION" "sparsehash-sparsehash-${SPARSEHASH_VERSION}"
 
-    if [ `ls -1 sparsehash-${SPARSEHASH_VERSION}.zip 2>/dev/null | wc -l` -le 0 ]
-    then
-        wget --no-check-certificate --output-document=sparsehash-${SPARSEHASH_VERSION}.zip $SPARSEHASH_LINK
-    fi
-
-    echo "...unpacking"
-    unzip -q -n sparsehash-${SPARSEHASH_VERSION}.zip
-    mv sparsehash-sparsehash-${SPARSEHASH_VERSION} sparsehash
-    
     echo "...fixing scripts"
 
-    cat sparsehash/src/windows/config.h | sed 's/stdext/std/g' | sed 's/<hash_map>/<unordered_map>/g' | sed 's/<hash_set>/<unordered_set>/g' > sparsehash/src/windows/config.h
-    cat sparsehash/src/windows/sparsehash/internal/sparseconfig.h | sed 's/stdext/std/g' | sed 's/<hash_map>/<unordered_map>/g' | sed 's/<hash_set>/<unordered_set>/g' > sparsehash/src/windows/sparsehash/internal/sparseconfig.h
-    cat sparsehash/src/windows/google/sparsehash/sparseconfig.h | sed 's/stdext/std/g' | sed 's/<hash_map>/<unordered_map>/g' | sed 's/<hash_set>/<unordered_set>/g' > sparsehash/src/windows/google/sparsehash/sparseconfig.h
+    for FILE in \
+            "sparsehash/src/windows/config.h" \
+            "sparsehash/src/windows/sparsehash/internal/sparseconfig.h" \
+            "sparsehash/src/windows/google/sparsehash/sparseconfig.h"
+    do
+        sed -i -e 's/stdext/std/g' -e 's/<hash_map>/<unordered_map>/g' -e 's/<hash_set>/<unordered_set>/g' "$FILE"
+    done
 
     cp -f -r sparsehash/src/windows/* sparsehash/src
 }
@@ -86,40 +76,22 @@ build_sparsehash()
 build_json()
 {
     echo "Building json"
-    JSON_VERSION=1.1.0
-    JSON_LINK=https://github.com/nlohmann/json/archive/v1.1.0.zip
+    local JSON_VERSION=1.1.0
+    local JSON_LINK="https://github.com/nlohmann/json/archive/v${JSON_VERSION}.zip"
 
-    echo "...cleaning"
-    rm -r -f json
-
-    if [ `ls -1 json-${JSON_VERSION}.zip 2>/dev/null | wc -l` -le 0 ]
-    then
-        wget --no-check-certificate --output-document=json-${JSON_VERSION}.zip $JSON_LINK
-    fi
-
-    echo "...unpacking"
-    unzip -q -n json-${JSON_VERSION}.zip
-    mv json-${JSON_VERSION} json
+    download_and_unzip "$JSON_LINK" "json" "$JSON_VERSION"
 }
 
 build_flatbuffers()
 {
     echo "Building FlatBuffers"
-    FLATBUFFERS_VERSION=1.3.0
-    FLATBUFFERS_LINK=https://github.com/google/flatbuffers/archive/v1.3.0.zip
+    local FLATBUFFERS_VERSION=1.3.0
+    local FLATBUFFERS_LINK="https://github.com/google/flatbuffers/archive/v${FLATBUFFERS_VERSION}.zip"
     
-    echo "...cleaning"
-    rm -r -f flatbuffers
+    download_and_unzip "$FLATBUFFERS_LINK" "flatbuffers" "$FLATBUFFERS_VERSION"
+
+    echo "...more cleaning"
     rm -r -f bin
-
-    if [ `ls -1 flatbuffers-$FLATBUFFERS_VERSION.zip 2>/dev/null | wc -l` -le 0 ]
-    then
-        wget --no-check-certificate --output-document=flatbuffers-$FLATBUFFERS_VERSION.zip $FLATBUFFERS_LINK
-    fi
-
-    echo "...unpacking"
-    unzip -q -n flatbuffers-$FLATBUFFERS_VERSION.zip
-    mv flatbuffers-$FLATBUFFERS_VERSION flatbuffers
     
     cd flatbuffers
     
@@ -137,20 +109,31 @@ build_flatbuffers()
 build_catch()
 {
     echo "Getting Catch"
-    CATCH_VERSION=1.5.4
-    CATCH_LINK="https://github.com/philsquared/Catch/archive/v${CATCH_VERSION}.zip"
+    local CATCH_VERSION=1.5.4
+    local CATCH_LINK="https://github.com/philsquared/Catch/archive/v${CATCH_VERSION}.zip"
 
-    local TARGET_DIR="catch"
-    local ARCHIVE_NAME="${TARGET_DIR}-${CATCH_VERSION}.zip"
+    download_and_unzip "$CATCH_LINK" "catch" "$CATCH_VERSION" "Catch-${CATCH_VERSION}"
+}
+
+download_and_unzip()
+{
+    local LINK="$1"
+    local TARGET_DIR="$2"
+    local VERSION="$3"
+    local UNPACKED_DIR="$4"  # Optional.
+
+    [ -n "$UNPACKED_DIR" ] || UNPACKED_DIR="${TARGET_DIR}-${VERSION}"
+
+    local ARCHIVE_NAME="${TARGET_DIR}-${VERSION}.zip"
 
     echo "...cleaning"
     rm -r -f "$TARGET_DIR"
 
-    redownload_package "$CATCH_LINK" "$ARCHIVE_NAME"
+    redownload_package "$LINK" "$ARCHIVE_NAME"
 
     echo "...unpacking"
-    unzip -q -n "$ARCHIVE_NAME"
-    mv Catch-${CATCH_VERSION} "$TARGET_DIR"
+    unzip -q -n "$ARCHIVE_NAME"  || die
+    mv "$UNPACKED_DIR" "$TARGET_DIR" || die
 }
 
 redownload_package()
@@ -160,9 +143,19 @@ redownload_package()
 
     if [ ! -f "$ARCHIVE_NAME" ]
     then
-        wget --no-check-certificate --output-document="${ARCHIVE_NAME}" "$PACKAGE_LINK"
+        wget --no-check-certificate --output-document="${ARCHIVE_NAME}" "$PACKAGE_LINK" || die
     fi
 }
+
+if [ -z "$1" ]
+then
+    echo "Nothing to build."
+    echo
+    echo "Usage:"
+    echo "  $0 <list> <of> <dependencies>"
+    echo "(use \"all\" to build all)"
+    exit 1
+fi
 
 for option
 do
@@ -192,6 +185,10 @@ do
             build_json
             build_flatbuffers
             build_catch
+        ;;
+        *)
+            echo "Unknown option '${option}'"
+            exit 1
         ;;
     esac
 done
