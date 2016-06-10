7f632c6558e1ccbab5813e7131e4bad6222c3336
Honza Sinkora <jan.sinkora@keenswh.com>
2016-06-10 13:20:41 +0200

    UI: Fixed build (missing using in MainForm)

--- Sources/UI/ArnoldUI/Forms/MainForm.cs
+++ Sources/UI/ArnoldUI/Forms/MainForm.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using ArnoldUI;
 using GoodAI.Arnold.Core;
