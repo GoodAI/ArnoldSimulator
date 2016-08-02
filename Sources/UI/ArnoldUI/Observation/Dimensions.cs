edf6386d4f9377215b967456593125e6936bdf6d
Premek Paska <premysl.paska@keenshw.com>
2016-08-02 15:47:48 +0200

    UI: Port TensorDimenstions from BrainSimulator.

---
+++ Sources/UI/ArnoldUI/Observation/Dimensions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodAI.Arnold.Observation
+{
+    public class InvalidDimensionsException : FormatException
+    {
+        public InvalidDimensionsException(string message) : base(message) { }
+    }
+
+    public class Dimensions
+    {
+        #region Static 
+
+        private static Dimensions m_emptyInstance;
+
+        public static Dimensions Empty => m_emptyInstance ?? (m_emptyInstance = new Dimensions());
+
+        #endregion
+
+        private readonly IImmutableList<int> m_dims;
+
+        public const int MaxDimensions = 100;  // Ought to be enough for everybody.
+
+        public Dimensions() : this(ImmutableList<int>.Empty)  // This means default dimensions.
+        { }
+
+        public Dimensions(IImmutableList<int> immutableDimensions)
+        {
+            if (immutableDimensions.Count > MaxDimensions)
+                throw new InvalidDimensionsException($"Maximum number of dimensions is {MaxDimensions}.");
+
+            m_dims = immutableDimensions;
+
+            // Precompute this since we are immutable.
+            ElementCount = IsEmpty ? 0 : Math.Abs(m_dims.Aggregate(1, (acc, item) => acc * item));  // Tolerate -1s.
+        }
+
+        public Dimensions(params int[] dimensions) : this(ProcessDimensions(dimensions))
+        { }
+
+        public Dimensions(IEnumerable<int> dimensions) : this(ProcessDimensions(dimensions))
+        { }
+
+        #region Object overrides
+
+        public bool Equals(Dimensions other)
+        {
+            if (other.Rank != Rank)
+                return false;
+
+            if ((Rank == 1) && (m_dims == null) && (other.m_dims == null))
+                return true;
+
+            return (m_dims != null) && (other.m_dims != null) && m_dims.SequenceEqual(other.m_dims);
+        }
+
+        public override int GetHashCode()
+        {
+            if (m_hashCode == -1)
+            {
+                m_hashCode = IsEmpty ? 0 : m_dims.Aggregate(19, (acc, item) => 31 * acc + item);
+            }
+
+            return m_hashCode;
+        }
+        private int m_hashCode = -1;
+
+        #endregion
+
+        public bool IsEmpty => (m_dims == null) || (m_dims.Count == 0);
+
+        public int Rank => IsEmpty ? 1 : m_dims.Count;
+
+        public int ElementCount { get; }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (IsEmpty)
+                {
+                    if (index == 0)
+                        return 0;  // We pretend we have one dimension of size 0.
+
+                    throw GetIndexOutOfRangeException(index, 0);
+                }
+
+                if (index >= m_dims.Count)
+                    throw GetIndexOutOfRangeException(index, m_dims.Count - 1);
+
+                return m_dims[index];
+            }
+        }
+
+        private static IndexOutOfRangeException GetIndexOutOfRangeException(int index, int maxIndex)
+        {
+            return new IndexOutOfRangeException($"Index {index} is greater than max index {maxIndex}.");
+        }
+
+        private static IImmutableList<int> ProcessDimensions(IEnumerable<int> dimensions)
+        {
+            ImmutableList<int>.Builder newDimensionsBuilder = ImmutableList.CreateBuilder<int>();
+
+            foreach (int item in dimensions)
+            {
+                if (item < 0)
+                    throw new InvalidDimensionsException($"Number {item} is not a valid dimension.");
+
+                newDimensionsBuilder.Add(item);
+
+                if (newDimensionsBuilder.Count > MaxDimensions)
+                    throw new InvalidDimensionsException($"Maximum number of dimensions is {MaxDimensions}.");
+            }
+
+            return newDimensionsBuilder.ToImmutable();
+        }
+    }
+}
---
+++ Sources/UI/Testing/ArnoldUITests/Observation/DimensionsTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GoodAI.Arnold.Observation;
+using Xunit;
+
+namespace GoodAI.Arnold.UI.Tests.Observation
+{
+    public class DimensionsTests
+    {
+        [Fact]
+        public void ElementCountIsCachedProperly()
+        {
+            var dims = new Dimensions(3, 5, 7);
+
+            Assert.Equal(3 * 5 * 7, dims.ElementCount);
+            Assert.Equal(3 * 5 * 7, dims.ElementCount);
+        }
+
+        [Fact]
+        public void HashCodeIsCachedProperly()
+        {
+            var dims = new Dimensions(3, 5, 7);
+
+            int hashCode = dims.GetHashCode();
+            Assert.Equal(hashCode, dims.GetHashCode());
+        }
+
+        [Fact]
+        public void EqualsTests()
+        {
+            Assert.True((new Dimensions(2, 3, 5)).Equals(new Dimensions(2, 3, 5)));
+            Assert.True((new Dimensions()).Equals(new Dimensions()));
+
+            Assert.False((new Dimensions(2, 3, 5)).Equals(new Dimensions(2, 3)));
+            Assert.False((new Dimensions(2, 3, 5)).Equals(new Dimensions(2, 3, 11)));
+
+            // ReSharper disable once SuspiciousTypeConversion.Global
+            Assert.False((new Dimensions(2, 3, 5)).Equals(0));  // Compare with some other value type.
+        }
+
+        [Fact]
+        public void RankReturnsNumberOfDims()
+        {
+            Assert.Equal(3, new Dimensions(5, 3, 2).Rank);
+        }
+
+        [Fact]
+        public void DefaultDimIsRankOneOfSizeZero()
+        {
+            Dimensions emptyDims = Dimensions.Empty;
+
+            Assert.Equal(1, emptyDims.Rank);
+            Assert.Equal(0, emptyDims[0]);
+            Assert.Equal(0, emptyDims.ElementCount);
+        }
+
+        [Fact]
+        public void AnyDimensionCanBeZero()
+        {
+            var rank1Dims = new Dimensions(0);
+            Assert.Equal(0, rank1Dims.ElementCount);
+
+            var rankNDims = new Dimensions(3, 0, 5);
+            Assert.Equal(3, rankNDims[0]);
+            Assert.Equal(0, rankNDims.ElementCount);
+        }
+
+        [Fact]
+        public void DefaultConstructorReturnsEmptyDims()
+        {
+            Assert.True(Dimensions.Empty.Equals(new Dimensions()));
+        }
+    }
+}
